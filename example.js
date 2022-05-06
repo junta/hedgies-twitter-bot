0b8a0ad53b38b97716fb2a6ab4cd4643d1ce4294
0xIbuki <junta8918@gmail.com>
2022-05-06 12:11:53 +0900

    change lastTime

--- example.js
+++ example.js
@@ -4,10 +4,12 @@ const list = require('./services/newListings');
 const stats = require('./services/dailyStats');
 const rarity = require('./services/getRarity');
 const dailyComp = require('./services/dailyComp');
+const requestEvents = require('./services/requestEvents');
 
 // tweet.tweet('✅  Hedgie Sold! \n \n 🦔');
 // list.newListings();
 // stats.tweetStats();
 // const result = rarity.getRarity('1965');
 // console.log(result.score);
-dailyComp.tweetDailyComp();
+// dailyComp.tweetDailyComp();
+requestEvents.request('successful');
