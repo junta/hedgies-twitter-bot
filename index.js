1439ed88a562e605ab7231fe0b5af446ba2b12fd
0xIbuki <junta8918@gmail.com>
2022-05-17 10:30:24 +0900

    install sentry.io

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const sale = require('./services/newSales');
 const list = require('./services/newListings');
 const stats = require('./services/dailyStats');
 const dailyComp = require('./services/dailyComp');
+require('dotenv').config();
+const Sentry = require('@sentry/node');
+const SentryTracing = require('@sentry/tracing');
+const { CaptureConsole } = require('@sentry/integrations');
+
+Sentry.init({
+	dsn: process.env.SENTRY_DNS,
+	integrations: [
+		new CaptureConsole({
+			// array of methods that should be captured
+			// defaults to ['log', 'info', 'warn', 'error', 'debug', 'assert']
+			levels: ['error']
+		})
+	],
+	tracesSampleRate: 1.0
+});
 
 cron.schedule('* * * * *', async () => {
 	list.newListings();
